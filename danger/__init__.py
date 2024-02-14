39e3471f83ffce91ab5d63469b31bc8f089e37bb
Ira Horecka <ira89@icloud.com>
2024-02-14 14:40:49 -0500

    rm danger/

--- danger/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-DANGER: Detection Algorithm for Network Group Enrichment Regions
-"""
-
-from danger.danger import SAFE
-
-__version__ = "0.0.1"
--- danger/stats/permutation/_cython/setup.py
+++
@@ -1,6 +0,0 @@
-# setup.py
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(ext_modules=cythonize("permutation.pyx"), include_dirs=[np.get_include()])
