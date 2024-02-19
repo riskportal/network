9c7be2012b869928810f75abf9d103ec4035fc58
Ira Horecka <ira89@icloud.com>
2024-02-18 20:28:40 -0500

    BIG UPDATE: automate dimple factor calculation

--- risk/risk.py
+++ risk/risk.py
@@ -1,9 +1,16 @@
+import os
+import sys
+from contextlib import contextmanager
 from pathlib import Path
 
+import numpy as np
 from rich import print
+from rich.progress import Progress
+from scipy import ndimage
 
 from risk.config import read_default_config, validate_config
 from risk.network.annotation import define_top_annotations
+from risk.network.graph import calculate_edge_lengths
 from risk.network.io import load_cys_network, load_network_annotation
 from risk.network.neighborhoods import (
     get_network_neighborhoods,
@@ -36,7 +43,7 @@ class SAFE:
         # TODO: What if the API is something like twosafe.load_cytoscape_network --> SAFE object
         self.network = self.load_cytoscape_network()
         self.annotation_map = self.load_network_annotation(self.network)
-        self.neighborhoods = self.load_neighborhoods(self.network)
+        self.network, self.neighborhoods = self.tune_graph_and_get_neighborhoods(self.network)
         self.neighborhood_enrichment_map = self.get_pvalues(self.neighborhoods, self.annotation_map)
         self.annotation_enrichment_matrix = self.define_top_annotations(
             self.network, self.annotation_map, self.neighborhood_enrichment_map
@@ -89,6 +96,24 @@ class SAFE:
             min_edges_per_node=network_min_edges_per_node,
             include_edge_weight=network_enrichment_include_edge_weight,
         )
+
+    def tune_graph_and_get_neighborhoods(self, G):
+        network_enrichment_compute_sphere = self.config["network_enrichment_compute_sphere"]
+        network_enrichment_dimple_factor = self.config["network_enrichment_dimple_factor"]
+        if network_enrichment_dimple_factor == "auto" and network_enrichment_compute_sphere:
+            # Dimple factors go from 0 (sphere surface) to 1000 (sphere center) - find best dimple factor within range
+            G_copy = self._get_network_with_best_dimples(
+                G, lower_bound=0, upper_bound=1024, tolerance=4
+            )
+        else:
+            G_copy = calculate_edge_lengths(
+                G.copy(),
+                compute_sphere=network_enrichment_compute_sphere,
+                include_edge_weight=self.config["network_enrichment_include_edge_weight"],
+                dimple_factor=network_enrichment_dimple_factor,
+            )
+        neighborhoods = self.load_neighborhoods(G_copy)
+        return G_copy, neighborhoods
 
     def load_network_annotation(self, network):
         print("[cyan]Loading [yellow]JSON[/yellow] [blue]network annotations[/blue]...")
@@ -158,7 +183,7 @@ class SAFE:
         ]
         group_distance_metric = self.config["group_distance_metric"]
         print(
-            f"[cyan]Defining [blue]domains[/blue] using the [yellow]'{group_distance_metric}'[/yellow] metric with [yellow]automatic[/yellow] distance thresholding..."
+            f"[cyan]Optimizing [blue]distance threshold[/blue] for [blue]domains[/blue] using the [yellow]'{group_distance_metric}'[/yellow] metric..."
         )
         domains_matrix = define_domains(
             neighborhood_enrichment_matrix,
@@ -201,6 +226,79 @@ class SAFE:
             neighborhood_binary_enrichment_matrix_below_alpha,
         )
 
+    def _get_network_with_best_dimples(self, G, lower_bound=0, upper_bound=500, tolerance=1):
+        print(
+            "[cyan][red]Warning:[/red] Optimizing [yellow]dimple factor[/yellow] can be an [red]expensive process[/red]."
+            "[blue] Mark down[/blue] [yellow]optimal dimple factor[/yellow] for future use...[/cyan]"
+        )
+        max_score = -np.inf
+        best_dimple_factor = lower_bound
+        best_graph = None
+
+        # Calculate total iterations for progress display
+        # Add one to account for 0 dimple factor testing
+        total_iterations = int(np.ceil(np.log2((upper_bound - lower_bound) / tolerance))) + 1
+        with Progress() as progress:
+            task_id = progress.add_task(
+                f"[cyan]Optimizing[/cyan] [yellow]dimple factor 0/{total_iterations}[/yellow]",
+                total=total_iterations,
+            )
+            current_iteration = 0
+
+            while upper_bound - lower_bound > tolerance:
+                if current_iteration == 0:
+                    # Explicitly test dimple factor of 0
+                    G_test_zero = calculate_edge_lengths(
+                        G.copy(),
+                        compute_sphere=self.config["network_enrichment_compute_sphere"],
+                        include_edge_weight=self.config["network_enrichment_include_edge_weight"],
+                        dimple_factor=0,
+                    )
+                    with suppress_print():
+                        neighborhoods_test_zero = self.load_neighborhoods(G_test_zero)
+                    score_test = ndimage.label(neighborhoods_test_zero)[1]
+                    if score_test > max_score:
+                        max_score = score_test
+                        best_dimple_factor = 0
+                        best_graph = G_test_zero
+                else:
+                    mid_dimple_factor = (lower_bound + upper_bound) / 2
+                    dimple_factors_to_test = [mid_dimple_factor, mid_dimple_factor + tolerance]
+
+                    # Always cast dimple_factors to type int
+                    for dimple_factor in map(lambda x: int(x), dimple_factors_to_test):
+                        G_test = calculate_edge_lengths(
+                            G.copy(),
+                            compute_sphere=self.config["network_enrichment_compute_sphere"],
+                            include_edge_weight=self.config[
+                                "network_enrichment_include_edge_weight"
+                            ],
+                            dimple_factor=dimple_factor,
+                        )
+                        with suppress_print():
+                            neighborhoods_test = self.load_neighborhoods(G_test)
+                        score_test = ndimage.label(neighborhoods_test)[1]
+
+                        if score_test > max_score:
+                            max_score = score_test
+                            best_dimple_factor = dimple_factor
+                            best_graph = G_test
+                    # Adjust the search bounds
+                    if best_dimple_factor == mid_dimple_factor + tolerance:
+                        lower_bound = mid_dimple_factor
+                    else:
+                        upper_bound = mid_dimple_factor
+
+                current_iteration += 1
+                progress.update(
+                    task_id,
+                    advance=1,
+                    description=f"[cyan]Optimizing[/cyan] [yellow]dimple factor {current_iteration}/{total_iterations}[/yellow]",
+                )
+
+        print(f"[yellow]Optimal dimple factor:[/yellow] [red]{best_dimple_factor}[/red]")
+        return best_graph
+
 
 def merge_configs(default_config, user_input):
     """
@@ -216,3 +314,14 @@ def merge_configs(default_config, user_input):
         if value:  # Check if the user_input value is truthy
             merged_config[key] = value  # Only overwrite if truthy
     return merged_config
+
+
+@contextmanager
+def suppress_print():
+    original_stdout = sys.stdout  # Save a reference to the original standard output
+    try:
+        with open(os.devnull, "w") as devnull:
+            sys.stdout = devnull  # Redirect the standard output to the null device
+            yield
+    finally:
+        sys.stdout = original_stdout  # Restore the standard output to its original value
