6a40b26b8d31f5596cf4a48b1eb24bdcdf748518
Ira Horecka <ira89@icloud.com>
2024-02-19 22:38:56 -0500

    add constants.py

---
+++ risk/network/constants.py
@@ -0,0 +1,28 @@
+# Define the group distance metrics available for silhouette scoring
+GROUP_DISTANCE_METRICS = [
+    "braycurtis",
+    "canberra",
+    "chebyshev",
+    "cityblock",
+    "correlation",
+    "cosine",
+    "dice",
+    "euclidean",
+    "hamming",
+    "jaccard",
+    "jensenshannon",
+    "kulczynski1",
+    "mahalanobis",
+    "matching",
+    "minkowski",
+    "rogerstanimoto",
+    "russellrao",
+    "seuclidean",
+    "sokalmichener",
+    "sokalsneath",
+    "sqeuclidean",
+    "yule",
+]
+
+# Define the linkage methods
+GROUP_LINKAGE_METHODS = ["single", "complete", "average", "weighted", "centroid", "median", "ward"]
