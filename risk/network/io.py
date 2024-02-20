7a10bf60c6520d3e8a1753b4dc7586adc8261003
Ira Horecka <ira89@icloud.com>
2024-02-19 22:38:00 -0500

    update comments

--- risk/network/io.py
+++ risk/network/io.py
@@ -95,7 +95,7 @@ def load_cys_network(
 
 
 def remove_invalid_graph_properties(G, min_edges_per_node=0):
-    # Remove nodes with 2 or fewer edges
+    # Remove nodes with `min_edges_per_node` or fewer edges
     nodes_with_few_edges = [node for node in G.nodes() if G.degree(node) <= min_edges_per_node]
     G.remove_nodes_from(nodes_with_few_edges)
     self_loops = list(nx.selfloop_edges(G))
