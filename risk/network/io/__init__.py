62094668afc5057ec3f88e72fb980dbab254f769
Ira Horecka <ira89@icloud.com>
2024-03-19 16:36:36 -0400

    expose alternative graph loading APIs. check for valid Graph object during io operation

--- risk/network/io/__init__.py
+++ risk/network/io/__init__.py
@@ -4,4 +4,4 @@ risk/network/io
 """
 
 from .annotation import load_network_annotation
-from .io import load_cys_network
+from .io import load_cys_network, load_gpickle_network, load_networkx_network
