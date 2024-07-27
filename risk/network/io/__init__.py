29c136568537bb807c59c273ea5e40527b8f7885
Ira Horecka <ira89@icloud.com>
2024-07-27 17:31:02 -0400

    big update: update io/ with new io-related files to work with new API

--- risk/network/io/__init__.py
+++ risk/network/io/__init__.py
@@ -3,5 +3,5 @@ risk/network/io
 ~~~~~~~~~~~~~~~
 """
 
-from .annotation import load_network_annotation
-from .io import load_cys_network, load_gpickle_network, load_networkx_network
+from .annotation import load_annotation
+from .network import NetworkIO
