d29ae15d39fdecc73d27a103a16c93f5b03fc115
Ira Horecka <ira89@icloud.com>
2024-07-30 01:41:45 -0400

    BIG CHECKPOINT: constants.py

---
+++ risk/constants.py
@@ -0,0 +1,31 @@
+"""
+risk/network/constants
+~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+GROUP_DISTANCE_METRICS = [
+    "braycurtis",
+    "canberra",
+    "chebyshev",
+    "cityblock",
+    "correlation",
+    "cosine",
+    "dice",
+    "euclidean",
+    "hamming",
+    "jaccard",
+    "jensenshannon",
+    "kulczynski1",
+    "mahalanobis",
+    "matching",
+    "minkowski",
+    "rogerstanimoto",
+    "russellrao",
+    "seuclidean",
+    "sokalmichener",
+    "sokalsneath",
+    "sqeuclidean",
+    "yule",
+]
+
+GROUP_LINKAGE_METHODS = ["single", "complete", "average", "weighted", "centroid", "median", "ward"]
