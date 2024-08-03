97da9070bf1c9099883141c89d6972ba17110222
Ira Horecka <ira89@icloud.com>
2024-08-03 17:00:57 -0400

    optimize cython libraries - variance --> stdev

--- risk/stats/permutation/_python/permutation.py
+++ risk/stats/permutation/_python/permutation.py
@@ -15,45 +15,39 @@ def compute_neighborhood_score_by_sum_python(neighborhoods_matrix, annotation_ma
     return neighborhood_score
 
 
-def compute_neighborhood_score_by_variance_python(neighborhoods_matrix, annotation_matrix):
-    # Convert to float64 for arithmetic operations, if not already
-    A = neighborhoods_matrix.astype(np.float64)
-    B = annotation_matrix
+def compute_neighborhood_score_by_stdev_python(neighborhoods_matrix, annotation_matrix):
     # Calculate neighborhood score (dot product of neighborhoods and annotations matrix)
-    neighborhood_score = np.dot(A, B)
-    # Sum across rows for A to get N, reshape for broadcasting
-    N = np.sum(A, axis=1)
+    neighborhood_score = np.dot(neighborhoods_matrix, annotation_matrix)
+    # Sum across rows for neighborhoods_matrix to get N, reshape for broadcasting
+    N = np.sum(neighborhoods_matrix, axis=1)
     N_reshaped = N[:, None]
     # Calculate mean of the dot product
     M = neighborhood_score / N_reshaped
     # Compute the mean of squares (EXX)
-    EXX = np.dot(A, np.power(B, 2)) / N_reshaped
+    EXX = np.dot(neighborhoods_matrix, np.power(annotation_matrix, 2)) / N_reshaped
     # Variance computation
-    # Variance = EXX - (Mean)^2
-    # Note: This directly computes the variance without standardizing by the standard deviation
     variance = EXX - np.power(M, 2)
-
-    return variance
+    # Standard deviation computation
+    stdev = np.sqrt(variance)
+    return stdev
 
 
 def compute_neighborhood_score_by_z_score_python(neighborhoods_matrix, annotation_matrix):
-    # Since there are no NaN values, we can directly use the annotation_matrix
-    A = neighborhoods_matrix
-    B = annotation_matrix
-    neighborhood_score = np.dot(A, B)  # sum of attribute values in a neighborhood
+    # Sum of attribute values in a neighborhood (dot product)
+    neighborhood_score = np.dot(neighborhoods_matrix, annotation_matrix)
     # Compute the number of elements in each neighborhood (assuming binary matrix for neighborhoods)
-    N = np.dot(A, np.ones(B.shape))  # No need to check for NaN, assuming all ones
+    N = np.dot(neighborhoods_matrix, np.ones(annotation_matrix.shape))
     # Calculate the mean values for each neighborhood
     M = neighborhood_score / N
-    # Use broadcasting to compute squared mean and mean of squares
-    EXX = np.dot(A, B**2) / N
-    EEX = M**2
+    # Compute squared mean and mean of squares
+    EXX = np.dot(neighborhoods_matrix, np.power(annotation_matrix, 2)) / N
+    EEX = np.power(M, 2)
     # Calculate standard deviation for each neighborhood
     std = np.sqrt(EXX - EEX)
     # Calculate Z-scores, handling division by zero by setting to NaN where std is 0
     with np.errstate(divide="ignore", invalid="ignore"):
-        neighborhood_score = np.divide(M, std)
-        neighborhood_score[std == 0] = np.nan  # Handle division by zero
-        neighborhood_score[N < 3] = np.nan  # Apply threshold for minimum number of elements
+        z_scores = np.divide(M, std)
+        z_scores[std == 0] = np.nan  # Handle division by zero
+        z_scores[N < 3] = np.nan  # Apply threshold for minimum number of elements
 
-    return neighborhood_score
+    return z_scores
