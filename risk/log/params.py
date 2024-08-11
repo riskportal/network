fd9f7ff0716e1ad7e6c61dd66d483fc00866f3b7
Ira Horecka <ira89@icloud.com>
2024-08-11 10:55:13 -0400

    update log/ with docstrings and type hints

--- risk/log/params.py
+++ risk/log/params.py
@@ -7,6 +7,7 @@ import csv
 import json
 import warnings
 from datetime import datetime
+from typing import Any
 
 import numpy as np
 
@@ -16,101 +17,166 @@ from .console import print_header
 warnings.filterwarnings("ignore")
 
 
+def _safe_param_export(func):
+    """A decorator to wrap parameter export functions in a try-except block for safe execution.
+
+    Args:
+        func (function): The function to be wrapped.
+
+    Returns:
+        function: The wrapped function with error handling.
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            filepath = (
+                kwargs.get("filepath") or args[1]
+            )  # Assuming filepath is always the second argument
+            print(f"Parameters successfully exported to filepath: {filepath}")
+            return result
+        except Exception as e:
+            filepath = kwargs.get("filepath") or args[1]
+            print(f"An error occurred while exporting parameters to {filepath}: {e}")
+            return None
+
+    return wrapper
+
+
 class Params:
+    """Handles the storage and logging of various parameters for network analysis.
+
+    This class provides methods to log parameters related to different components of the analysis,
+    such as the network, annotations, neighborhoods, graph, and plotter settings. It also stores
+    the current datetime when the parameters were initialized.
+    """
+
     def __init__(self):
+        """Initialize the Params object with default settings and current datetime."""
         self.initialize()
         self.datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    def initialize(self):
+    def initialize(self) -> None:
+        """Initialize the parameter dictionaries for different components."""
         self.network = {}
         self.annotations = {}
         self.neighborhoods = {}
         self.graph = {}
         self.plotter = {}
 
-    def log_network(self, **kwargs):
+    def log_network(self, **kwargs) -> None:
+        """Log network-related parameters.
+
+        Args:
+            **kwargs: Network parameters to log.
+        """
         self.network = {**self.network, **kwargs}
 
-    def log_annotations(self, **kwargs):
+    def log_annotations(self, **kwargs) -> None:
+        """Log annotation-related parameters.
+
+        Args:
+            **kwargs: Annotation parameters to log.
+        """
         self.annotations = {**self.annotations, **kwargs}
 
-    def log_neighborhoods(self, **kwargs):
+    def log_neighborhoods(self, **kwargs) -> None:
+        """Log neighborhood-related parameters.
+
+        Args:
+            **kwargs: Neighborhood parameters to log.
+        """
         self.neighborhoods = {**self.neighborhoods, **kwargs}
 
-    def log_graph(self, **kwargs):
+    def log_graph(self, **kwargs) -> None:
+        """Log graph-related parameters.
+
+        Args:
+            **kwargs: Graph parameters to log.
+        """
         self.graph = {**self.graph, **kwargs}
 
-    def log_plotter(self, **kwargs):
+    def log_plotter(self, **kwargs) -> None:
+        """Log plotter-related parameters.
+
+        Args:
+            **kwargs: Plotter parameters to log.
+        """
         self.plotter = {**self.plotter, **kwargs}
 
-    def to_csv(self, filepath):
-        try:
-            # Load the parameter dictionary
-            params = self.load()
-            # Find the union of all inner dictionary keys to use as columns
-            columns = set()
-            for nested_dict in params.values():
-                columns.update(nested_dict.keys())
-            columns = list(columns)
-            # Open the file in write mode
-            with open(filepath, "w", newline="") as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=["id"] + columns)
-                # Write the header
-                writer.writeheader()
-                # Write the rows
-                for key, nested_dict in params.items():
-                    row = {"id": key}
-                    row.update(nested_dict)
-                    writer.writerow(row)
+    @_safe_param_export
+    def to_csv(self, filepath: str) -> None:
+        """Export the parameters to a CSV file.
 
-            print(f"Parameters successfully exported to filepath: {filepath}")
-        except Exception as e:
-            print(f"An error occurred while exporting the parameter: {e}")
+        Args:
+            filepath (str): The path where the CSV file will be saved.
+        """
+        # Load the parameter dictionary
+        params = self.load()
+        # Open the file in write mode
+        with open(filepath, "w", newline="") as csv_file:
+            writer = csv.writer(csv_file)
+            # Write the header
+            writer.writerow(["parent_key", "child_key", "value"])
+            # Write the rows
+            for parent_key, parent_value in params.items():
+                if isinstance(parent_value, dict):
+                    for child_key, child_value in parent_value.items():
+                        writer.writerow([parent_key, child_key, child_value])
+                else:
+                    writer.writerow([parent_key, "", parent_value])
 
-    def to_json(self, filepath):
-        try:
-            with open(filepath, "w") as json_file:
-                json.dump(self.load(), json_file, indent=4)
-            print(f"Parameters successfully exported to filepath: {filepath}")
-        except Exception as e:
-            print(f"An error occurred while exporting the parameter: {e}")
+    @_safe_param_export
+    def to_json(self, filepath: str) -> None:
+        """Export the parameters to a JSON file.
 
-    def to_txt(self, filepath):
-        try:
-            # Load the parameter dictionary
-            params = self.load()
-            # Open the file in write mode
-            with open(filepath, "w") as txt_file:
-                for key, nested_dict in params.items():
-                    # Write the key
-                    txt_file.write(f"{key}:\n")
-                    # Write the nested dictionary values, one per line
-                    for nested_key, nested_value in nested_dict.items():
-                        txt_file.write(f"  {nested_key}: {nested_value}\n")
-                    # Add a blank line between different keys
-                    txt_file.write("\n")
+        Args:
+            filepath (str): The path where the JSON file will be saved.
+        """
+        with open(filepath, "w") as json_file:
+            json.dump(self.load(), json_file, indent=4)
 
-            print(f"Parameters successfully exported to filepath: {filepath}")
-        except Exception as e:
-            print(f"An error occurred while exporting the parameter: {e}")
+    @_safe_param_export
+    def to_txt(self, filepath: str) -> None:
+        """Export the parameters to a text file.
 
-    def load(self):
+        Args:
+            filepath (str): The path where the text file will be saved.
+        """
+        # Load the parameter dictionary
+        params = self.load()
+        # Open the file in write mode
+        with open(filepath, "w") as txt_file:
+            for key, nested_dict in params.items():
+                # Write the key
+                txt_file.write(f"{key}:\n")
+                # Write the nested dictionary values, one per line
+                for nested_key, nested_value in nested_dict.items():
+                    txt_file.write(f"  {nested_key}: {nested_value}\n")
+                # Add a blank line between different keys
+                txt_file.write("\n")
+
+    def load(self) -> dict:
+        """Load and process various parameters, converting any np.ndarray values to lists.
+
+        Returns:
+            dict: A dictionary containing the processed parameters.
+        """
         print_header("Loading parameters")
         return _convert_ndarray_to_list(
             {
-                "datetime": self.datetime,
-                "network": self.network,
                 "annotations": self.annotations,
-                "neighborhoods": self.neighborhoods,
+                "datetime": self.datetime,
                 "graph": self.graph,
+                "neighborhoods": self.neighborhoods,
+                "network": self.network,
                 "plotter": self.plotter,
             }
         )
 
 
-def _convert_ndarray_to_list(d):
-    """
-    Recursively convert all np.ndarray values in the dictionary to lists.
+def _convert_ndarray_to_list(d: Any) -> Any:
+    """Recursively convert all np.ndarray values in the dictionary to lists.
 
     Args:
         d (dict): The dictionary to process.
@@ -119,10 +185,14 @@ def _convert_ndarray_to_list(d):
         dict: The processed dictionary with np.ndarray values converted to lists.
     """
     if isinstance(d, dict):
+        # Recursively process each value in the dictionary
         return {k: _convert_ndarray_to_list(v) for k, v in d.items()}
     elif isinstance(d, list):
+        # Recursively process each item in the list
         return [_convert_ndarray_to_list(v) for v in d]
     elif isinstance(d, np.ndarray):
+        # Convert numpy arrays to lists
         return d.tolist()
     else:
+        # Return the value unchanged if it's not a dict, list, or ndarray
         return d
