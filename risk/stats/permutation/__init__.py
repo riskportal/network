256a95dff6d9a8e0c6a4dd3ee79030cfe3b937ae
Ira Horecka <ira89@icloud.com>
2024-08-12 16:29:52 -0400

    udpate risk/stats

--- risk/stats/permutation/__init__.py
+++ risk/stats/permutation/__init__.py
@@ -3,12 +3,12 @@ risk/stats/permutation
 ~~~~~~~~~~~~~~~~~~~~~~
 """
 
-from ._cython.permutation import (
+from risk.stats.permutation._cython.permutation import (
     compute_neighborhood_score_by_sum_cython,
     compute_neighborhood_score_by_stdev_cython,
     compute_neighborhood_score_by_z_score_cython,
 )
-from ._python.permutation import (
+from risk.stats.permutation._python.permutation import (
     compute_neighborhood_score_by_sum_python,
     compute_neighborhood_score_by_stdev_python,
     compute_neighborhood_score_by_z_score_python,
