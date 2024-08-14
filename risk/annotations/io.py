1a6d951d52c2469f3c34713747e57c02bd23056b
Ira Horecka <ira89@icloud.com>
2024-08-14 15:12:54 -0400

    update annotation io params

--- risk/annotations/io.py
+++ risk/annotations/io.py
@@ -52,28 +52,29 @@ class AnnotationsIO:
         label_colname: str = "label",
         nodes_colname: str = "nodes",
         sheet_name: str = "Sheet1",
-        delimiter: str = ";",
+        nodes_delimiter: str = ";",
     ) -> Dict[str, Any]:
-        """Load annotations from an Excel file and convert them to a dictionary.
+        """Load annotations from an Excel file and associate them with the network.
 
         Args:
             filepath (str): Path to the Excel annotations file.
-            network (NetworkX graph): The network to which the annotations are related.
-            label_colname (str): Name of the column containing the labels.
-            nodes_colname (str): Name of the column containing the nodes.
-            sheet_name (str): The name of the Excel sheet to load (default is 'Sheet1').
-            delimiter (str): Delimiter used to parse the nodes column (default is ';').
+            network (nx.Graph): The NetworkX graph to which the annotations are related.
+            label_colname (str): Name of the column containing the labels (e.g., GO terms).
+            nodes_colname (str): Name of the column containing the nodes associated with each label.
+            sheet_name (str, optional): The name of the Excel sheet to load (default is 'Sheet1').
+            nodes_delimiter (str, optional): Delimiter used to separate multiple nodes within the nodes column (default is ';').
 
         Returns:
-            dict: A dictionary where each label is paired with its respective list of nodes.
+            Dict[str, Any]: A dictionary where each label is paired with its respective list of nodes,
+                            linked to the provided network.
         """
         filetype = "Excel"
         params.log_annotations(filepath=filepath, filetype=filetype)
         _log_loading(filetype, filepath=filepath)
         # Load the specified sheet from the Excel file
         df = pd.read_excel(filepath, sheet_name=sheet_name)
-        # Split the nodes column by the specified delimiter
-        df[nodes_colname] = df[nodes_colname].apply(lambda x: x.split(delimiter))
+        # Split the nodes column by the specified nodes_delimiter
+        df[nodes_colname] = df[nodes_colname].apply(lambda x: x.split(nodes_delimiter))
         # Convert the DataFrame to a dictionary pairing labels with their corresponding nodes
         label_node_dict = df.set_index(label_colname)[nodes_colname].to_dict()
         return load_annotations(network, label_node_dict)
@@ -84,25 +85,28 @@ class AnnotationsIO:
         network: nx.Graph,
         label_colname: str = "label",
         nodes_colname: str = "nodes",
-        delimiter: str = ";",
+        nodes_delimiter: str = ";",
     ) -> Dict[str, Any]:
-        """Load annotations from a CSV file and convert them to a DataFrame.
+        """Load annotations from a CSV file and associate them with the network.
 
         Args:
             filepath (str): Path to the CSV annotations file.
-            network (NetworkX graph): The network to which the annotations are related.
-            label_colname (str): Name of the column containing the labels.
-            nodes_colname (str): Name of the column containing the nodes.
-            delimiter (str): Delimiter used to parse the nodes column (default is ';').
+            network (nx.Graph): The NetworkX graph to which the annotations are related.
+            label_colname (str): Name of the column containing the labels (e.g., GO terms).
+            nodes_colname (str): Name of the column containing the nodes associated with each label.
+            nodes_delimiter (str, optional): Delimiter used to separate multiple nodes within the nodes column (default is ';').
 
         Returns:
-            pd.DataFrame: DataFrame containing the labels and parsed nodes.
+            Dict[str, Any]: A dictionary where each label is paired with its respective list of nodes,
+                            linked to the provided network.
         """
         filetype = "CSV"
         params.log_annotations(filepath=filepath, filetype=filetype)
         _log_loading(filetype, filepath=filepath)
         # Load the CSV file into a dictionary
-        annotations_input = _load_matrix_file(filepath, label_colname, nodes_colname, delimiter)
+        annotations_input = _load_matrix_file(
+            filepath, label_colname, nodes_colname, delimiter=",", nodes_delimiter=nodes_delimiter
+        )
         # Process and return the annotations in the context of the network
         return load_annotations(network, annotations_input)
 
@@ -112,47 +116,56 @@ class AnnotationsIO:
         network: nx.Graph,
         label_colname: str = "label",
         nodes_colname: str = "nodes",
+        nodes_delimiter: str = ";",
     ) -> Dict[str, Any]:
-        """Load annotations from a TSV file and convert them to a DataFrame.
+        """Load annotations from a TSV file and associate them with the network.
 
         Args:
             filepath (str): Path to the TSV annotations file.
-            network (NetworkX graph): The network to which the annotations are related.
-            label_colname (str): Name of the column containing the labels.
-            nodes_colname (str): Name of the column containing the nodes.
+            network (nx.Graph): The NetworkX graph to which the annotations are related.
+            label_colname (str): Name of the column containing the labels (e.g., GO terms).
+            nodes_colname (str): Name of the column containing the nodes associated with each label.
+            nodes_delimiter (str, optional): Delimiter used to separate multiple nodes within the nodes column (default is ';').
 
         Returns:
-            pd.DataFrame: DataFrame containing the labels and parsed nodes.
+            Dict[str, Any]: A dictionary where each label is paired with its respective list of nodes,
+                            linked to the provided network.
         """
         filetype = "TSV"
         params.log_annotations(filepath=filepath, filetype=filetype)
         _log_loading(filetype, filepath=filepath)
-        # Load the TSV file with tab delimiter and convert to dictionary
+        # Load the TSV file into a dictionary
         annotations_input = _load_matrix_file(
-            filepath, label_colname, nodes_colname, delimiter="\t"
+            filepath, label_colname, nodes_colname, delimiter="\t", nodes_delimiter=nodes_delimiter
         )
         # Process and return the annotations in the context of the network
         return load_annotations(network, annotations_input)
 
 
 def _load_matrix_file(
-    filepath: str, label_colname: str, nodes_colname: str, delimiter: str = ";"
+    filepath: str,
+    label_colname: str,
+    nodes_colname: str,
+    delimiter: str = ",",
+    nodes_delimiter: str = ";",
 ) -> Dict[str, Any]:
     """Load annotations from a CSV or TSV file and convert them to a dictionary.
 
     Args:
         filepath (str): Path to the annotation file.
-        label_colname (str): Name of the column containing the labels.
-        nodes_colname (str): Name of the column containing the nodes.
-        delimiter (str): Delimiter used to parse the nodes column (default is ';').
+        label_colname (str): Name of the column containing the labels (e.g., GO terms).
+        nodes_colname (str): Name of the column containing the nodes associated with each label.
+        delimiter (str, optional): Delimiter used to separate columns in the file (default is ',').
+        nodes_delimiter (str, optional): Delimiter used to separate multiple nodes within the nodes column (default is ';').
 
     Returns:
-        dict: A dictionary where each label is paired with its respective list of nodes.
+        Dict[str, Any]: A dictionary where each label is paired with its respective list of nodes.
     """
-    df = pd.read_csv(filepath)
-    # Split the nodes column by the delimiter
-    df[nodes_colname] = df[nodes_colname].apply(lambda x: x.split(delimiter))
-    # Create a dictionary pairing labels with their corresponding nodes
+    # Load the CSV or TSV file into a DataFrame
+    df = pd.read_csv(filepath, delimiter=delimiter)
+    # Split the nodes column by the nodes_delimiter to handle multiple nodes per label
+    df[nodes_colname] = df[nodes_colname].apply(lambda x: x.split(nodes_delimiter))
+    # Create a dictionary pairing labels with their corresponding list of nodes
     label_node_dict = df.set_index(label_colname)[nodes_colname].to_dict()
     return label_node_dict
 
