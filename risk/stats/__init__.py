f29ed5233357395f5a007af720f0d84fccad0688
Ira Horecka <ira89@icloud.com>
2024-08-30 17:58:59 +0900

    big update: include fishers exact test and hypergeometric test to suite of statistical tests

--- risk/stats/__init__.py
+++ risk/stats/__init__.py
@@ -3,4 +3,7 @@ risk/stats
 ~~~~~~~~~~
 """
 
-from .stats import calculate_significance_matrices, compute_permutation
+from .stats import calculate_significance_matrices
+from .fisher_exact import compute_fisher_exact_test
+from .hypergeom import compute_hypergeom_test
+from .permutation import compute_permutation
