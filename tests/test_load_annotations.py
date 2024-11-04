c33d601ef01cc79b577d948eec4b08520970da57
Ira Horecka <ira89@icloud.com>
2024-11-04 14:06:47 -0500

    update tests/

--- tests/test_load_annotations.py
+++ tests/test_load_annotations.py
@@ -14,7 +14,9 @@ def test_load_csv_annotation(risk_obj, cytoscape_network, data_path):
     """
     annotation_file = data_path / "csv" / "annotations" / "go_biological_process.csv"
     annotations = risk_obj.load_csv_annotation(
-        filepath=str(annotation_file), network=cytoscape_network
+        filepath=str(annotation_file),
+        network=cytoscape_network,
+        min_nodes_per_term=1,
     )
 
     assert annotations is not None
@@ -29,7 +31,11 @@ def test_load_dict_annotation(risk_obj, cytoscape_network, annotation_dict):
         cytoscape_network: The network object to which annotations will be applied.
         annotation_dict: A dictionary containing annotations.
     """
-    annotations = risk_obj.load_dict_annotation(content=annotation_dict, network=cytoscape_network)
+    annotations = risk_obj.load_dict_annotation(
+        content=annotation_dict,
+        network=cytoscape_network,
+        min_nodes_per_term=1,
+    )
 
     assert annotations is not None
     assert len(annotations) > 0  # Check that annotations are loaded.
@@ -45,7 +51,9 @@ def test_load_json_annotation(risk_obj, cytoscape_network, data_path):
     """
     annotation_file = data_path / "json" / "annotations" / "go_biological_process.json"
     annotations = risk_obj.load_json_annotation(
-        filepath=str(annotation_file), network=cytoscape_network
+        filepath=str(annotation_file),
+        network=cytoscape_network,
+        min_nodes_per_term=1,
     )
 
     assert annotations is not None
@@ -62,7 +70,9 @@ def test_load_tsv_annotation(risk_obj, cytoscape_network, data_path):
     """
     annotation_file = data_path / "tsv" / "annotations" / "go_biological_process.tsv"
     annotations = risk_obj.load_tsv_annotation(
-        filepath=str(annotation_file), network=cytoscape_network
+        filepath=str(annotation_file),
+        network=cytoscape_network,
+        min_nodes_per_term=1,
     )
 
     assert annotations is not None
@@ -79,7 +89,9 @@ def test_load_excel_annotation(risk_obj, cytoscape_network, data_path):
     """
     annotation_file = data_path / "excel" / "annotations" / "go_biological_process.xlsx"
     annotations = risk_obj.load_excel_annotation(
-        filepath=str(annotation_file), network=cytoscape_network
+        filepath=str(annotation_file),
+        network=cytoscape_network,
+        min_nodes_per_term=1,
     )
 
     assert annotations is not None
