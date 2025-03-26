33a66bab012049f98f8f387799dd997fd3a771db
Ira Horecka <ira89@icloud.com>
2025-03-26 19:44:06 -0400

    update stat_tests.py --> tests.py

--- risk/stats/__init__.py
+++ risk/stats/__init__.py
@@ -4,12 +4,11 @@ risk/stats
 """
 
 from risk.stats.permutation import compute_permutation_test
-from risk.stats.stat_tests import (
+from risk.stats.significance import calculate_significance_matrices
+from risk.stats.tests import (
     compute_binom_test,
     compute_chi2_test,
     compute_hypergeom_test,
     compute_poisson_test,
     compute_zscore_test,
 )
-
-from risk.stats.significance import calculate_significance_matrices
